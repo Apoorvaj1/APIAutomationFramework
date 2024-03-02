1fe94d14158cf8a2674f5f454408f19d655eeb20
apoorv jain <apoorv127@gmail.com>
2024-03-02 15:15:58 +0530

    02nd march

---
+++ APIAutomationFramework/src/test/java/com/API/Automation/base/BaseTest.java
@@ -0,0 +1,43 @@
+package com.API.Automation.base;
+
+
+import com.API.Automation.actions.AssertActions;
+import com.API.Automation.endpoints.APIConstants;
+import com.API.Automation.modules.PayloadManager;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+
+    public RequestSpecification requestSpecification;
+    public AssertActions assertActions;
+    public PayloadManager payloadManager;
+    public JsonPath jsonPath;
+    public Response response;
+    public ValidatableResponse validatableResponse;
+
+
+    @BeforeTest
+    public void setConfig(){
+        payloadManager = new PayloadManager();
+        assertActions = new AssertActions();
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(APIConstants.BASE_URL)
+                .addHeader("Content-Type","application/json")
+                .build().log().all();
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ APIAutomationFramework/src/test/java/com/API/Automation/tests/e2e/TC1.java
@@ -0,0 +1,6 @@
+package com.API.Automation.tests.e2e;
+
+public class TC1 {
+
+    // Create Booking -> Update Booking -> Delete Booking
+}
